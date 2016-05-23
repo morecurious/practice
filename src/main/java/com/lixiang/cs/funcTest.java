77d21af94815047ff2053a4897a527cb292e4e68
lixiang <good396975802@qq.com>
2016-05-23 23:21:18 +0800

    功能测试函数

---
+++ src/main/java/com/lixiang/cs/funcTest.java
@@ -0,0 +1,10 @@
+package com.lixiang.cs;
+
+/**
+ * Created by lixiang on 2016/5/23.
+ */
+public class funcTest {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/lixiang/cs/util/Classfiy.java
+++
@@ -1,7 +0,0 @@
-package com.lixiang.cs.util;
-
-/**
- * Created by root on 16-5-21.
- */
-public class Classfiy {
-}
