2facdc04c88ad2d7f1d3fbf1734e8fa9a01657c4
lixiang <good396975802@qq.com>
2016-08-03 19:35:46 +0800

    用dubbo.properties代替 Spring.xml做配置

--- practice-consumer/src/main/java/com/dubbo/demo/testConsumer.java
+++ practice-consumer/src/main/java/com/dubbo/demo/testConsumer.java
@@ -2,16 +2,19 @@ package com.dubbo.demo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by lixiang on 2016/8/3.
  */
 public class testConsumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         applicationContext.start();
 
         DemoService demoService = (DemoService) applicationContext.getBean("demoService");
 
         System.out.println(demoService.say());
+        System.in.read();
     }
 }
